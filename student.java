8c8e6a3f1874ec28ca8f8daf1cd19a8dff47e033
Juan Esteban Avila <juanavila311026@gmail.com>
2025-04-25 17:29:21 -0500

    Create student.java

---
+++ student.java
@@ -0,0 +1,45 @@
+public class estudiante {
+    private String nombre;
+    private int edad;
+    private int codigo;
+    private float promedio;
+
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setEdad (int edad){
+        this.edad = edad;
+    }
+    
+    public void setCodigo (int codigo){
+        this.codigo = codigo;
+    }
+    
+    public void setProm (float promedio){
+        this.promedio = promedio;
+    }
+    
+    public String getNombre (){
+        return nombre;
+    }
+
+    public int  getEdad (){
+        return edad;
+    }
+
+    public int  getCodigo (){
+        return codigo;
+    }
+
+    public float getPromedio (){
+        return promedio;
+    }
+
+}
+
+
+
+
+
+
