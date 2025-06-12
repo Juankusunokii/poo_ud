8d5ee73aa381756c610999b0f0c5c77ae9c32d28
Juan Esteban Avila <juanavila311026@gmail.com>
2025-06-12 17:33:54 -0500

    Update UDstudent.java

--- UDstudent.java
+++ UDstudent.java
@@ -29,3 +29,86 @@ public class UDstudent extends estudiante {
     }
 
 }
+public class Engine {
+    public Integer cC;
+    public Integer Price;
+    public Integer Weight = 200;
+
+    public Integer calculatedPrice(){
+        
+        if ( this.cC < 1600 && this.cC > 1200 ){
+            this.price = 10000000;
+        }
+        else if (cC < 1200){
+            this.price = 8000000;
+        }
+        else if (cC > 1600){
+            this.price = 12000000;
+        }
+        else {
+            System.out.println("Pregunte por su preferencia");
+        }
+        return this.Price;
+    }
+
+    public Integer calculatedWeight(){
+         
+        if ( this.cC < 1600 && this.cC > 1200){
+            this.weight += 15;
+        }
+        else if (cC < 1200){
+            this.Weight += 12; 
+        }
+        else if (cC > 1600){
+            this.Weight = 20;
+        }
+        return this.Weight;
+    }
+}
+public class Chassis {
+
+    public String type;
+    public Integer price;
+
+
+    public Integer calculatePrice(){
+        if (this.type == "deportivo"){
+            this.price += 5000000;
+        }
+        else if (this.type == "classic"){
+            this.price += 1000000;
+        }
+        return this.price;
+    }
+    
+}
+import Chassis;
+import Engine;
+
+
+public class Car{
+
+    public String color;
+
+    private void calcualatedPrice(){
+
+    }
+    
+    public string toString(){
+
+    }
+}
+import Car;
+import util.java.Scanner;
+
+public class Catalog {
+
+    public static void printMenu() {
+
+        System.out.println("===== Welcome to  =====");
+        System.out.println("Choose an option:");
+        
+    }
+    
+    
+}
